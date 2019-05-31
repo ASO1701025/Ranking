9d52c95741caab02ecb9f9411b80f22315980014
ASO1701025 <1701025@st.asojuku.ac.jp>
2019-05-31 11:59:06 +0900

    aaa

--- resources/views/new.php
+++ resources/views/new.php
@@ -1,284 +1,48 @@
-
-
 <?php
-/**
- * Created by PhpStorm.
- * User: daichi
- * Date: 2019/05/24
- * Time: 11:47
- */ ?>
-
-<html>
+// デバイス判定
+if (strpos($_SERVER['HTTP_USER_AGENT'], 'iPhone') !== false) {
+    if ($_REQUEST['view'] == 'pc') {
+        readfile('index_pc.html');
+    } else
+        readfile('index_touch.html');
+}
+else {
+    // readfile('index_pc.html');
+}
+?>
+<!DOCTYPE HTML>
+<html lang="ja">
 <head>
-    <meta charset="utf-8"/>
-    <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
-    <title>iPhone 6/7/8 – 3</title>
-    <style id="applicationStylesheet" type="text/css">
-        body {
-            margin: 0;
-            padding: 0;
-        }
-        :root {
-            --web-view-ids: iPhone_6_7_8___3;
-        }
-        #iPhone_6_7_8___3 * {
-            margin: 0;
-            padding: 0;
-        }
-        #iPhone_6_7_8___3 {
-            position: absolute;
-            box-sizing: border-box;
-            background: #E5E5E5;
-            width: 375px;
-            height: 667px;
-            background-color: rgba(255,255,255,1);
-            overflow: hidden;
-            margin: 0;
-            padding: 0;
-            opacity: 1;
-            --web-view-name: iPhone 6/7/8 – 3;
-            --web-view-id: iPhone_6_7_8___3;
-            --web-enable-deep-linking: true;
-        }
-        #____ {
-            opacity: 1;
-            position: absolute;
-            left: 128px;
-            top: 100px;
-            box-sizing: border-box;
-            margin: 0;
-            padding: 0;
-            overflow: visible;
-            width: 121px;
-            white-space: nowrap;
-            text-align: left;
-            font-family: Verdana;
-            font-style: normal;
-            font-weight: normal;
-            font-size: 30px;
-            color: rgba(7,7,7,1);
-        }
-        #USERNAME {
-            opacity: 1;
-            position: absolute;
-            left: 45px;
-            top: 227px;
-            box-sizing: border-box;
-            margin: 0;
-            padding: 0;
-            overflow: visible;
-            width: 69px;
-            white-space: nowrap;
-            text-align: left;
-            font-family: Verdana;
-            font-style: normal;
-            font-weight: normal;
-            font-size: 12px;
-            color: rgba(7,7,7,1);
-        }
-        #____2 {
-            opacity: 1;
-            fill: rgba(255,255,255,1);
-            stroke: rgb(112, 112, 112);
-            stroke-width: 1px;
-            stroke-linejoin: miter;
-            stroke-linecap: butt;
-            stroke-miterlimit: 4;
-            shape-rendering: auto;
-        }
-        .____2 {
-            position: absolute;
-            overflow: visible;
-            width: 197px;
-            height: 31px;
-            left: 138px;
-            top: 219px;
-        }
-        #USERID {
-            opacity: 1;
-            position: absolute;
-            left: 45px;
-            top: 269px;
-            box-sizing: border-box;
-            margin: 0;
-            padding: 0;
-            overflow: visible;
-            width: 48px;
-            white-space: nowrap;
-            text-align: left;
-            font-family: Verdana;
-            font-style: normal;
-            font-weight: normal;
-            font-size: 12px;
-            color: rgba(7,7,7,1);
-        }
-        #PASSWORD {
-            opacity: 1;
-            position: absolute;
-            left: 45px;
-            top: 311px;
-            box-sizing: border-box;
-            margin: 0;
-            padding: 0;
-            overflow: visible;
-            width: 71px;
-            white-space: nowrap;
-            text-align: left;
-            font-family: Verdana;
-            font-style: normal;
-            font-weight: normal;
-            font-size: 12px;
-            color: rgba(7,7,7,1);
-        }
-        #PASSWORD_Confirmation {
-            opacity: 1;
-            position: absolute;
-            left: 43px;
-            top: 353px;
-            box-sizing: border-box;
-            margin: 0;
-            padding: 0;
-            overflow: visible;
-            width: 79px;
-            white-space: nowrap;
-            text-align: left;
-            font-family: Verdana;
-            font-style: normal;
-            font-weight: normal;
-            font-size: 12px;
-            color: rgba(7,7,7,1);
-        }
-        #____4 {
-            opacity: 1;
-            fill: rgba(255,255,255,1);
-            stroke: rgb(112, 112, 112);
-            stroke-width: 1px;
-            stroke-linejoin: miter;
-            stroke-linecap: butt;
-            stroke-miterlimit: 4;
-            shape-rendering: auto;
-        }
-        .____4 {
-            position: absolute;
-            overflow: visible;
-            width: 197px;
-            height: 31px;
-            left: 138px;
-            top: 261px;
-        }
-        #____5 {
-            opacity: 1;
-            fill: rgba(255,255,255,1);
-            stroke: rgb(112, 112, 112);
-            stroke-width: 1px;
-            stroke-linejoin: miter;
-            stroke-linecap: butt;
-            stroke-miterlimit: 4;
-            shape-rendering: auto;
-        }
-        .____5 {
-            position: absolute;
-            overflow: visible;
-            width: 197px;
-            height: 31px;
-            left: 138px;
-            top: 303px;
-        }
-        #____6 {
-            opacity: 1;
-            fill: rgba(255,255,255,1);
-            stroke: rgb(112, 112, 112);
-            stroke-width: 1px;
-            stroke-linejoin: miter;
-            stroke-linecap: butt;
-            stroke-miterlimit: 4;
-            shape-rendering: auto;
-        }
-        .____6 {
-            position: absolute;
-            overflow: visible;
-            width: 197px;
-            height: 31px;
-            left: 138px;
-            top: 353px;
-        }
-        #__ {
-            opacity: 1;
-            position: absolute;
-            left: 168px;
-            top: 530px;
-            box-sizing: border-box;
-            margin: 0;
-            padding: 0;
-            overflow: visible;
-            width: 41px;
-            white-space: nowrap;
-            text-align: left;
-            font-family: Verdana;
-            font-style: normal;
-            font-weight: normal;
-            font-size: 20px;
-            color: rgba(7,7,7,1);
-        }
-        #top {
-            opacity: 1;
-            position: absolute;
-            left: 172px;
-            top: 584px;
-            box-sizing: border-box;
-            margin: 0;
-            padding: 0;
-            overflow: visible;
-            width: 33px;
-            white-space: nowrap;
-            text-align: left;
-            font-family: Verdana;
-            font-style: normal;
-            font-weight: normal;
-            font-size: 20px;
-            color: rgba(7,7,7,1);
-        }
-    </style>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Instant Ranking 新規登録画面</title>
+    <link rel="stylesheet" href="style.css">
+
+    <!-- Bootstrap読み込み（スタイリングのため） -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css">
 </head>
 <body>
-<div id="iPhone_6_7_8___3">
-    <div id="____">
-        <span>新規登録</span>
-    </div>
-    <div id="USERNAME">
-        <span>USERNAME</span>
-    </div>
-    <svg class="____2">
-        <rect id="____2" rx="0" ry="0" x="0" y="0" width="197" height="31">
-        </rect>
-    </svg>
-    <div id="USERID">
-        <span>USERID</span>
-    </div>
-    <div id="PASSWORD">
-        <span>PASSWORD</span>
-    </div>
-    <div id="PASSWORD_Confirmation">
-        <span>PASSWORD<br/>Confirmation</span>
-    </div>
-    <svg class="____4">
-        <rect id="____4" rx="0" ry="0" x="0" y="0" width="197" height="31">
-        </rect>
-    </svg>
-    <svg class="____5">
-        <rect id="____5" rx="0" ry="0" x="0" y="0" width="197" height="31">
-        </rect>
-    </svg>
-    <svg class="____6">
-        <rect id="____6" rx="0" ry="0" x="0" y="0" width="197" height="31">
-        </rect>
-    </svg>
-    <div id="__">
-        <span>登録</span>
-    </div>
-    <div id="top">
-        <span>top</span>
-    </div>
+<div class="col-xs-6 col-xs-offset-3">
+
+    <form method="post">
+        <h1>会員登録フォーム</h1>
+        <div class="form-group">
+            <input type="text" class="form-control" name="username" placeholder="ユーザー名" required />
+        </div>
+
+        <div class="form-group">
+            <input type="email"  class="form-control" name="email" placeholder="メールアドレス" required />
+        </div>
+        <div class="form-group">
+            <input type="password" class="form-control" name="password" placeholder="パスワード" required />
+        </div>
+        <div class="form-group">
+            <input type="password" class="form-control" name="password" placeholder="パスワードの再確認" required />
+        </div>
+        <button type="submit" class="btn btn-default" name="signup">新規登録</button>
+        <a href="index.php">ログインはこちら</a>
+        <a href="Top.php">topへ戻る</a>
+    </form>
 </div>
 </body>
 </html>
