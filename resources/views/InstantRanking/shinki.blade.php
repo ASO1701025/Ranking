0735a6fddfd6af3f2d23aba653c887042f4a3ae7
ASO1701025 <1701025@st.asojuku.ac.jp>
2019-07-05 12:01:32 +0900

    test

--- resources/views/InstantRanking/shinki.blade.php
+++ resources/views/InstantRanking/shinki.blade.php
@@ -36,15 +36,15 @@ if (strpos($_SERVER['HTTP_USER_AGENT'], 'iPhone') !== false) {
 <body>
 
 <?php if (isset($_POST['username'])){
-    echo $_POST['username'];
-    echo $_POST['email'];
-    echo $_POST['password'];
+    $user =  $_POST['username'];
+    $mail =   $_POST['email'];
+    $pass =  $_POST['password'];
 
   $res = null;$pg_conn = null;$sql = null;$date = null;
         date_default_timezone_set('Asia/Tokyo');$date = date("Y-m-d");
         $pg_conn = pg_connect("host=ec2-174-129-240-67.compute-1.amazonaws.com port=5432 dbname=d8hdi8o0nv2hqq user=idiprlkaujoahf password=b1459a0b24b0e4d1334f38a9a2cb9f81ad0a1ba719639bfb7e9b1ac0efd601ef");
         if( $pg_conn ) {
-            $sql = "INSERT INTO userinformation (username,password,address) VALUES({$_POST['username']},{$_POST['password']},{$_POST['email']})";
+            $sql = "INSERT INTO userinformation (username,password,address) VALUES($user,$pass,$mail)";
             var_dump($sql);
             $res = pg_query( $pg_conn, $sql);var_dump($res);
             echo "接続できたよ！";
