43a3a5af25cffa3eb635ffdace0dcb6472260ada
ASO1701025 <1701025@st.asojuku.ac.jp>
2019-07-10 12:26:36 +0900

    select

--- resources/views/InstantRanking/login.blade.php
+++ resources/views/InstantRanking/login.blade.php
@@ -36,7 +36,7 @@
                 session_regenerate_id(true); //session_idを新しく生成し、置き換える
                 $_SESSION['email'] = $_POST['email'];
                 echo "ログインできたよ！";
-                var_dump(pg_fetch_all($res));
+                echo pg_fetch_result($res,0);
                 echo $_SESSION['email'];
 
             }
