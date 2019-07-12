68db4b59889063a11e18fe4de6433fd04d868162
ASO1701025 <1701025@st.asojuku.ac.jp>
2019-07-12 12:36:34 +0900

    セッション

--- resources/views/InstantRanking/home.blade.php
+++ resources/views/InstantRanking/home.blade.php
@@ -111,7 +111,7 @@
 
         <?
         $request = request();
-        $request->session()->reflash();
+        session()->replace(session()->all());
         echo $request->session()->get('name');
         ?>
     </div>
