fedc1a2b25b50d5711fb2e2ba5f29296cee0780d
ASO1701025 <1701025@st.asojuku.ac.jp>
2019-07-16 11:50:46 +0900

    セッション

--- resources/views/InstantRanking/top.blade.php
+++ resources/views/InstantRanking/top.blade.php
@@ -36,7 +36,7 @@ if (strpos($_SERVER['HTTP_USER_AGENT'], 'iPhone') !== false) {
                     echo "<br/>";
                 }
                 ?>
-                <img class="logo" src="./hukurou.png" alt="logo">
+                <img alt="テスト" src{{asset('/img/hukurou.png')}} width="340" height="470"><br>
 
             </span>
             <!-- 空白 -->
