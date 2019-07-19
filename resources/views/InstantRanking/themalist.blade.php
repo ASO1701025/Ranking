0870dadb3e6232e86f2fe89cbc13571cbce705d8
ASO1701025 <1701025@st.asojuku.ac.jp>
2019-07-19 12:08:35 +0900

    TE-MA

--- resources/views/InstantRanking/themalist.blade.php
+++ resources/views/InstantRanking/themalist.blade.php
@@ -93,7 +93,7 @@
             if($pg_conn){
 
                 //これを取得する分に書き換(,)
-                $sql = "SELECT * FROM WHERE address='$email' AND password='$password'";
+                $sql = "SELECT * FROM userinformation  WHERE  address = '$email' AND password = '$password'";
                 var_dump($sql);
 
                 if($res = pg_query($pg_conn, $sql)){
