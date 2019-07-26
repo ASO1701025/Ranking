aa473045a7c9c752cdc2a6013963a455f03c483a
ASO1701025 <1701025@st.asojuku.ac.jp>
2019-07-26 09:49:55 +0900

    mmm

--- resources/views/InstantRanking/themalist.blade.php
+++ resources/views/InstantRanking/themalist.blade.php
@@ -154,7 +154,7 @@
                         //session_regenerate_id(true);//session_idを新しく生成し、置き換え
                         $nemu = pg_fetch_array($res);
 
-                            var_dump($nemu["1"]);
+                            echo($nemu["1"]);
                         }
                     ?>
                 </div>
