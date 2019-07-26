f84560c6f3d6c8a515852b5251e4e6782fc5cdb5
ASO1701025 <1701025@st.asojuku.ac.jp>
2019-07-26 11:36:44 +0900

    mmm

--- resources/views/InstantRanking/top.blade.php
+++ resources/views/InstantRanking/top.blade.php
@@ -36,7 +36,7 @@ if (strpos($_SERVER['HTTP_USER_AGENT'], 'iPhone') !== false) {
                     echo "<br/>";
                 }
                 ?>
-                <img class="img-circle" src="hukurou.png" alt="logo"><br>
+                <img alt="テスト" src{{asset('hukurou.png')}} width="340" height="470"><br>
 
             </span>
             <!-- 空白 -->
