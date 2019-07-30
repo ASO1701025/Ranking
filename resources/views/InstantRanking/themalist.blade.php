ade61bbb2dec3fd9720b4118d428ed258bb02807
ASO1701025 <1701025@st.asojuku.ac.jp>
2019-07-30 12:21:00 +0900

    m

--- resources/views/InstantRanking/themalist.blade.php
+++ resources/views/InstantRanking/themalist.blade.php
@@ -170,7 +170,7 @@
                         echo '<div class="card" onclick="frameClick()">';
                         echo $hoge["$i"]["themavalue"];
                         echo '</div>';
-                        if($i>=0){
+                        if($j=0){
                             $i--;
                         }
                         ?>
