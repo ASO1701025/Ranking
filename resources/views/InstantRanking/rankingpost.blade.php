18d66d181b09c4de9a04b49eb6a156ff6d0efaba
ASO1701025 <1701025@st.asojuku.ac.jp>
2019-07-31 09:36:26 +0900

    8

--- resources/views/InstantRanking/rankingpost.blade.php
+++ resources/views/InstantRanking/rankingpost.blade.php
@@ -20,20 +20,25 @@
 </head>
 <body>
 
-<?php if (!isset($_POST['ranpos'])){ ?>
+<?php
+if (isset($_COOKIE["un"])) {
+    $user = $_COOKIE["un"];
+    print "<p>";
+    //print "ユーザネーム：".$_COOKIE["un"];
+    print "</p>";
+}
+?>
+
+<?php if (!isset($_POST['thema'])){ ?>
 
 <header id="caption"align="center"><h1>InstantRanking</h1></header>
 <form method="post">
     {{ csrf_field() }}
     <div align="center">
-        <p>ジャンル
-            <select name="genre">
-                <option value="1">食べ物</option>
-                <option value="2">レジャー</option>
-            </select>
+        <p>テーマ<br>
+            <textarea name="thema" rows="2" cols="30" wrap="soft"></textarea>
         </p>
-        <p>テーマ
-            //ジャンルに合わせて登録されているテーマを表示
+        <p>ジャンル
             <select name="genre">
                 <option value="1">食べ物</option>
                 <option value="2">レジャー</option>
@@ -46,15 +51,27 @@
     <?php   }else{
 
         $res = null;$pg_conn = null;$sql = null;$date = null;
-        date_default_timezone_set('Asia/Tokyo');$date = date("Y-m-d");
+        date_default_timezone_set('Asia/Tokyo');
+
+
+//            $date = date("Y/m/d H:i:s");
+//            $time = $date("Y/m/d H:i:s", strtotime('+14 day'));
+
+        $time = date("Y/m/d H:i:s", strtotime('+14 day')) . "\n";
+        $thema = $_POST['thema'];
+        $genle = $_POST['genre'];
+
         $pg_conn = pg_connect("host=ec2-174-129-240-67.compute-1.amazonaws.com port=5432 dbname=d8hdi8o0nv2hqq user=idiprlkaujoahf password=b1459a0b24b0e4d1334f38a9a2cb9f81ad0a1ba719639bfb7e9b1ac0efd601ef");
 
         if( $pg_conn ) {
 
 
-            $sql = "INSERT INTO userinformation (themavalue,genrelid) VALUES('{$thema}' , '{$genle}')";
+
+            $sql = "INSERT INTO thema (themavalue,genrelid,timelimit,userid) VALUES('{$thema}' , '{$genle}' , '{$time}' , '{$user}' )";
 
             var_dump($sql);
+
+            header('Location: ./home');
 
             $res = pg_query( $pg_conn, $sql);	var_dump($res);
             echo "接続できたよ！";
@@ -66,7 +83,7 @@
             pg_close($pg_conn);}
 
     };
-    echo "テーマが追加されました！";
+    //echo "テーマが追加されました！";
 
 
 
